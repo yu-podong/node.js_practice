b6883fa3d99353557547d1cd6fba8581a1f8112e
yu-podong <yu01gun@naver.com>
2021-08-20 13:32:53 +0900

    Day 9 : 8/20 update

---
+++ day9/exam01_memo/src/store/index.js
@@ -0,0 +1,61 @@
+import { createStore } from 'vuex'
+import axios from 'axios'
+
+export default createStore({
+  state: {
+    memoList : [],
+    bNowWriting: false
+  },
+  mutations: {
+    updateMemoList(state, payload) {
+      state.memoList = payload;
+    }
+  },
+  actions: {
+    async updateMemoList(context) {
+      try {
+        // Object 중 필요한 것만 selecting하는 것
+        let {status, data} = await axios.get('http://localhost:8000/api/v1/memo/find/skip/0/limit/100');
+
+        console.log(status);
+        console.log(data);
+
+        context.commit('updateMemoList', data.d);
+      }
+      catch(e) {
+        console.log(e);
+      }
+    },
+    async writeMemo(context, payload) {
+      context.state.bNowWriting = true;
+
+      let _res = await axios({
+        method: 'POST',
+        url: 'http://localhost:8000/api/v1/memo/insert',
+        data: JSON.stringify(payload),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      context.state.bNowWriting = false;
+    },
+    async deleteMemo(context, payload) {
+      try {
+        // Object 중 필요한 것만 selecting하는 것
+        let {status, data} = await axios.get(`http://localhost:8000/api/v1/memo/delete/id/${payload}`);
+
+        console.log(status);
+        console.log(data);
+
+        context.dispatch('updateMemoList');
+        //context.commit('updateMemoList', data.d);
+      }
+      catch(e) {
+        console.log(e);
+      }
+    }
+  },
+  modules: {
+  }
+})
---
+++ day9/exam01_memo/src/views/Memo.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<h1>Memo</h1>
+		<button @click="updateMemoList"> update </button>
+		<ul>
+			<li v-for="(item, index) in memoList" :key="index">
+				<h1>{{item.title}}</h1>
+				<h3>{{item.text}}</h3>
+				<button @click="deleteMemo(item._id)"> delete </button>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Memo',
+	computed : {
+		memoList() {
+			return this.$store.state.memoList;
+		}
+	},
+	methods : {
+		updateMemoList() {
+			this.$store.dispatch('updateMemoList');
+		},
+		deleteMemo(_id) {
+			console.log(_id);
+			this.$store.dispatch('deleteMemo', _id);
+		}
+	},
+	created() {
+		console.log('create');
+		this.$store.dispatch('updateMemoList');
+	}
+}
+</script>
+
+<style>
+
+</style>
